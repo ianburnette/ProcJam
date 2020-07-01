60081dd3f40f497f4f1d0559ca200d920cf101be
ianburnette <ianburnette@yandex.com>
2020-07-01 16:29:25 +0200

    scene setup

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+    [SerializeField] Vector3 rotationPosition;
+    [SerializeField] float rotationSpeed;
+    void Update() {
+        transform.RotateAround(rotationPosition, Vector3.up, rotationSpeed);
+        transform.LookAt(rotationPosition);
+    }
+}
