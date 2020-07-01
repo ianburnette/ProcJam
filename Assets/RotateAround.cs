ab41d6113d0691e2b968982a28db3a53e25dd58d
ianburnette <ianburnette@yandex.com>
2020-07-01 16:41:29 +0200

    additional setup

--- Assets/RotateAround.cs
+++ Assets/RotateAround.cs
@@ -1,12 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class RotateAround : MonoBehaviour {
+    [SerializeField] RotationDirection direction;
     [SerializeField] Vector3 rotationPosition;
     [SerializeField] float rotationSpeed;
+    Vector3 rotationDirection;
+    
+    void OnEnable() {
+        switch (direction) {
+            case RotationDirection.X: rotationDirection = Vector3.right; break;
+            case RotationDirection.Y: rotationDirection = Vector3.up; break;
+            case RotationDirection.Z: rotationDirection = Vector3.forward; break;
+        }
+    }
+
     void Update() {
-        transform.RotateAround(rotationPosition, Vector3.up, rotationSpeed);
+        transform.RotateAround(rotationPosition, rotationDirection, rotationSpeed);
         transform.LookAt(rotationPosition);
     }
 }
+
+[Serializable]
+public enum RotationDirection { X, Y, Z }
